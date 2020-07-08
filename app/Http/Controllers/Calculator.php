3ebc8e1fb179c7bb7d831bdc7929c205bea76968
Soulyaris <genessa174@gmail.com>
2020-07-08 18:15:59 +0500

    calculator formula and function update

--- app/Http/Controllers/Calculator.php
+++ app/Http/Controllers/Calculator.php
@@ -7,29 +7,29 @@ use App\Http\Requests\CalculatorRequest;
 
 class Calculator extends Controller
 {
-    public function calc(CalculatorRequest $request) {
-
-        function countPercents($month, $globalData) {
-            if ($month == 1) {
-                return $globalData['sum'];
+    private function countPercents($month, $globalData) {
+        if ($month == 1) {
+            return $globalData['sum'];
+        } else {
+            $previousMonth = $this->countPercents($month - 1, $globalData);
+            //receiving date values
+            $curDate = date_create_from_format('m/d/Y', $globalData['date']);
+            $curDate = $curDate->modify('+'.strval($month).' months');
+            $curDate = date_format($curDate, 'd-m-Y');
+            $curDate = strtotime($curDate);
+            $curDateMonth = date("n", $curDate);
+            $curDateYear = date("Y", $curDate);
+            if (date("L", $curDate)) {
+            $curYearDays = 365;
             } else {
-                $previousMonth = countPercents($month - 1, $globalData);
-                //receiving date values
-                $curDate = date_create_from_format('m/d/Y', $globalData['date']);
-                $curDate = $curDate->modify('+'.strval($month).' months');
-                $curDate = date_format($curDate, 'd-m-Y');
-                $curDate = strtotime($curDate);
-                $curDateMonth = date("n", $curDate);
-                $curDateYear = date("Y", $curDate);
-                if (date("L", $curDate)) {
-                $curYearDays = 365;
-                } else {
-                $curYearDays = 366;
-                };
-                //----
-                return $previousMonth + ($previousMonth + $globalData['replenishSum']) * cal_days_in_month(CAL_GREGORIAN, $curDateMonth, $curDateYear) * ($globalData['percent'] / $curYearDays);
-            }
+            $curYearDays = 366;
+            };
+            //----
+            return $previousMonth + $globalData['replenishSum'] + ($previousMonth + $globalData['replenishSum']) * cal_days_in_month(CAL_GREGORIAN, $curDateMonth, $curDateYear) * ($globalData['percent'] / $curYearDays);
         }
+    }
+
+    public function calc(CalculatorRequest $request) {
 
         $date = $request->input("calculator-date");
         $sum = intval($request->input("calculator-sum-range"));
@@ -37,9 +37,8 @@ class Calculator extends Controller
         $replenish = $request->input("calculator-replenish");
         $replenishSum = intval($request->input("calculator-replenish-sum"));
 
-        //$summn = $summn - 1 + ($summn - 1 + $summadd) * $daysn * ($percent / $daysy);
         if ($replenish == false) {$replenishSum = 0;};
-        $percent = 10;
+        $percent = 10 / 100;
 
         $globalData = [
             'sum' => $sum,
@@ -47,7 +46,7 @@ class Calculator extends Controller
             'percent' => $percent,
             'date' => $date
         ];
-        $percentsSum = countPercents($time * 12, $globalData);
+        $percentsSum = $this->countPercents($time * 12, $globalData);
         echo json_encode(round($percentsSum, 2));
     }
 }
